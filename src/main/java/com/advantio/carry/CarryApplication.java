ff62ba1abfb732c0617ee1711cd954dea35251f7
Antonio <antonioalvino95@gmail.com>
2019-05-03 21:38:37 +0200

    First Commit - CarRy Archetype

---
+++ src/main/java/com/advantio/carry/CarryApplication.java
@@ -0,0 +1,13 @@
+package com.advantio.carry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarryApplication.class, args);
+	}
+
+}
