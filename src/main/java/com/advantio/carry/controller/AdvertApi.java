683e302548a32765d7d68d8c1ea5a0dfd258b975
Antonio <antonioalvino95@gmail.com>
2019-05-04 19:50:37 +0200

    RESTful get all Adverts

--- src/main/java/com/advantio/carry/controller/AdvertApi.java
+++ src/main/java/com/advantio/carry/controller/AdvertApi.java
@@ -1,5 +1,7 @@
 package com.advantio.carry.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +41,10 @@ public class AdvertApi {
 			advertDao.save(advert);
 			return new ResponseEntity<>(HttpStatus.OK);
 		}
+	}
+
+	@GetMapping("/allAdverts")
+	public List<Advert> allAdverts() {
+		return (List<Advert>) advertDao.findAll();
 	}
 }
